f82b7c3e39f4573bfa0209ab4fa20a9c2773cb3f
Damilare Ajayi <117741632+Damilareajayi@users.noreply.github.com>
2026-02-28 04:27:59 -0500

    Update storage.ts

--- server/storage.ts
+++ server/storage.ts
@@ -1,32 +1,28 @@
-import { db } from "./db";
-import { grants, faculty, type Grant, type InsertGrant, type Faculty, type InsertFaculty } from "@shared/schema";
+import { grants, faculty, users, type Grant, type InsertGrant, type Faculty, type User, type InsertUser } from "@shared/schema";
 
 export interface IStorage {
   getGrants(): Promise<Grant[]>;
-  createGrant(grant: InsertGrant): Promise<Grant>;
   getFaculty(): Promise<Faculty[]>;
-  createFaculty(faculty: InsertFaculty): Promise<Faculty>;
+  // Add other methods if your app needs them
 }
 
-export class DatabaseStorage implements IStorage {
+export class MemStorage implements IStorage {
+  private _grants: Map<number, Grant>;
+  private _faculty: Map<number, Faculty>;
+
+  constructor() {
+    this._grants = new Map();
+    this._faculty = new Map();
+  }
+
   async getGrants(): Promise<Grant[]> {
-    return await db.select().from(grants);
+    return Array.from(this._grants.values());
   }
-  async createGrant(grant: InsertGrant): Promise<Grant> {
-    const [created] = await db.insert(grants).values(grant).returning();
-    return created;
-  }
+
   async getFaculty(): Promise<Faculty[]> {
-    return await db.select().from(faculty);
-  }
-  async createFaculty(fac: InsertFaculty): Promise<Faculty> {
-    const [created] = await db.insert(faculty).values(fac).returning();
-    return created;
+    return Array.from(this._faculty.values());
   }
 }
 
-// Look for these lines at the bottom:
-// export const storage = new DatabaseStorage(); 
-
-// Change it to this:
+// This is the line that was causing the ReferenceError
 export const storage = new MemStorage();
