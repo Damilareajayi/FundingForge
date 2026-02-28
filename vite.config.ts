f13cc3678636f796498fec2af7d4eab43b143e04
Damilare Ajayi <117741632+Damilareajayi@users.noreply.github.com>
2026-02-28 11:40:07 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "./",
+  base: "/jupyterlab/default/proxy/5000/",
   plugins: [react()],
   resolve: {
     alias: {
